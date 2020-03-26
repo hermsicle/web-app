8bbaf32cc08a8b7d2d7969695d0300f5f4824391
Herman Liu <hermanliu95@gmail.com>
2020-03-26 01:37:14 -0700

    added modals for other flip cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,26 @@ document.querySelector('.demo-btn')
 document.querySelector('#x-btn')
     .addEventListener('click', toggleModal);
 
+
+const toggleModal2 = () => {
+    document.querySelector('.modal2')
+        .classList.toggle('modal2--hidden')
+}
+
+document.querySelector('.demo-btn2')
+    .addEventListener('click', toggleModal2);
+
+document.querySelector('#x-btn2')
+    .addEventListener('click', toggleModal2);
+
+
+const toggleModal3 = () => {
+    document.querySelector('.modal3')
+        .classList.toggle('modal3--hidden')
+}
+
+document.querySelector('.demo-btn3')
+    .addEventListener('click', toggleModal3);
+
+document.querySelector('#x-btn3')
+    .addEventListener('click', toggleModal3);
